a461f2514245bdcc0c138ac5e6d859323dc64df8
SPArrowX666 <mxu2492@gmail.com>
2025-07-11 13:27:48 +0900

    Update hotel.py

--- hotel.py
+++ hotel.py
@@ -37,18 +37,24 @@ try:
     fig, ax = plt.subplots(figsize=(12, 6))
     sns.barplot(x=df["Score"], y=hotel_names, palette="coolwarm", ax=ax)
     ax.set(title="Score by Hotel", xlabel="Score", ylabel="Hotel Name")
+    if ax.get_legend() is not None:
+        ax.get_legend().remove()
     st.pyplot(fig)
 
     
     fig, ax = plt.subplots(figsize=(12, 6))
     sns.barplot(x=df["Discount (%)"], y=hotel_names, palette="YlGnBu", ax=ax)
     ax.set(title="Discount Rate (%) by Hotel", xlabel="Discount Rate (%)", ylabel="Hotel Name")
+    if ax.get_legend() is not None:
+        ax.get_legend().remove()
     st.pyplot(fig)
 
     
     fig, ax = plt.subplots(figsize=(12, 6))
     sns.barplot(x=df["Price (Yen)"], y=hotel_names, palette="OrRd", ax=ax)
     ax.set(title="Minimum Price (Yen) by Hotel", xlabel="Price (Yen)", ylabel="Hotel Name")
+    if ax.get_legend() is not None:
+        ax.get_legend().remove()
     st.pyplot(fig)
 
 except Exception:
